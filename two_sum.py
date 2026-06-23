a310b058abdddeeca908a2860604c9371e66a359
Soundar Krishna <soundarkrishna7@gmail.com>
2026-06-23 15:32:49 +0530

    Day 1: Two Sum brute force solution

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,7 @@
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i] + nums[j] == target:
+                return[i , j]
 
+print(two_sum([2, 7, 11, 15], 9))
