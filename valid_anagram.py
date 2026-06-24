01c3eabb8db60d45d0706a4958212617795b1dfc
Soundar Krishna <soundarkrishna7@gmail.com>
2026-06-24 23:12:38 +0530

    Day 2: Valid Anagram solution

--- valid_anagram.py
+++ valid_anagram.py
@@ -8,11 +8,12 @@ def valid_anagram(s, t):
         else:
             count[s[i]] = 1
     for i in range(len(t)):
-        if t[i] in count:
+        if t[i] in count and count[t[i]] > 0:
             count[t[i]] = count[t[i]] - 1
         else:
             return False
     return True
 
-print(valid_anagram('anagram', 'nagaram'))
-print(valid_anagram('rat', 'car'))
+print(valid_anagram('anagram', 'nagaram'))   # expected: True
+print(valid_anagram('rat', 'car'))            # expected: False
+print(valid_anagram('aacc', 'ccac'))          # expected: False (the one that broke earlier)
